30715366972983caef305617c7fbdd3ce37c88f4
kolexstrings <koladeolukoya@gmail.com>
2023-10-20 03:31:04 +0100

    feat: token smart contract added

--- contracts/KoladeToken.sol
+++ contracts/KoladeToken.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
-// A library is like a contract with reusable code, which can be called by other contracts.
-// Deploying common code can reduce gas costs.
-library ConvertLib{
-	function convert(uint amount, uint conversionRate) public pure returns (uint convertedAmount)
-	{
-		return amount * conversionRate;
-	}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract KoladeToken is ERC20 {
+    constructor() ERC20("Kolade Token", "KT") {
+        _mint(msg.sender, 1000000 * 10 ** uint(decimals()));
+    }
 }
