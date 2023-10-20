7d6d04b60da78da354fd62d636271ad36076621d
kolexstrings <koladeolukoya@gmail.com>
2023-10-20 12:03:17 +0100

    perf: configure dynamic loading of abi and address on redeployment

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,14 +1,35 @@
 const KoladeToken = artifacts.require("KoladeToken");
 const KoladeUserFactory = artifacts.require("KoladeUserFactory");
+const fs = require('fs');
 
 module.exports = async function (deployer) {
   await deployer.deploy(KoladeToken);
   const koladeTokenInstance = await KoladeToken.deployed();
 
+  const koladeTokenABI = koladeTokenInstance.abi;
+  const koladeTokenAddress = koladeTokenInstance.address;
+
+  const koladeTokenData = {
+    tokenContractAbi: koladeTokenABI,
+    tokenContractAddress: koladeTokenAddress
+  }
+
   console.log("KoladeToken has been successfully deployed to: ", koladeTokenInstance.address)
 
   await deployer.deploy(KoladeUserFactory, koladeTokenInstance.address);
   const koladeUserFactoryInstance = await KoladeUserFactory.deployed();
+  
+  const koladeUserFactoryABI = koladeUserFactoryInstance.abi;
+  const koladeUserFactoryAddress = koladeUserFactoryInstance.address;
+
+
+  const koladeUserFactoryData = {
+    factoryContractAbi: koladeUserFactoryABI,
+    factoryContractAddress: koladeUserFactoryAddress
+  }
+
+  fs.writeFileSync('../build/koladeTokenData.json', JSON.stringify(koladeTokenData, null, 2));
+  fs.writeFileSync('../build/koladeUserFactoryData', JSON.stringify(koladeUserFactoryData, null, 2));
 
   console.log("KoladeUserFactory has been successfully depployed to: ", koladeUserFactoryInstance.address);
 
